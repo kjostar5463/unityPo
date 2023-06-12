03b6a9a4236a3bb3b105930838308ce4b9d6b45a
kjostar5463 <kkrc5463@gmail.com>
2023-06-12 19:37:57 +0900

    6/12

---
+++ My project/Assets/Script/PhotonSetting.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonSetting : MonoBehaviour
+{
+    [SerializeField] InputField email;
+    [SerializeField] InputField userID;
+    [SerializeField] InputField password;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
